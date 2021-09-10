469a990b1f577fd65074ddb004b6d7998c65e903
lilacse <rirakusu@protonmail.com>
2021-09-10 15:13:05 +0800

    Initial main page script

---
+++ src/scripts/main.ts
@@ -0,0 +1,73 @@
+const equipOptionsSlotsInput = document.getElementById("equipOptionsSlotsInput");
+const equipOptionsFailedInput = document.getElementById("equipOptionsFailedInput");
+const equipOptionsCostPerClickInput = document.getElementById("equipOptionsCostPerClickInput");
+
+const equipOptionsResetButton = document.getElementById("equipOptionsResetButton");
+
+const simOptionsFailsToInnoInput = document.getElementById("simOptionsFailsToInnoInput");
+const simOptionsUseArkInnoInput = document.getElementById("simOptionsUseArkInnoInput");
+const simOptionsUseSpellTraceInnoInput = document.getElementById("simOptionsUseSpellTraceInnoInput");
+const simOptionsUseSpellTraceCssInput = document.getElementById("simOptionsUseSpellTraceCssInput");
+const simOptionsCssCostInput = document.getElementById("simOptionsCssCostInput");
+const simOptionsInnoCostInput = document.getElementById("simOptionsInnoCostInput");
+const simOptionsTraceCostInput = document.getElementById("simOptionsTraceCostInput");
+const simOptionsUseCssTypeInput = document.getElementById("simOptionsUseCssTypeInput");
+const simOptionsUseInnoTypeInput = document.getElementById("simOptionsUseInnoTypeInput");
+const simOptionsTraceTypeInput = document.getElementById("simOptionsTraceTypeInput");
+const simOptionsHasFeverInput = document.getElementById("simOptionsHasFeverInput");
+const simOptionsHasDiscountInput = document.getElementById("simOptionsHasDiscountInput");
+const simOptionsDiligenceLevelInput = document.getElementById("simOptionsDiligenceLevelInput");
+const simOptionsGuildEnhancementMasteryBonusInput = document.getElementById("simOptionsGuildEnhancementMasteryBonusInput");
+const simOptionsGuildUpgradeSalvationLevelInput = document.getElementById("simOptionsGuildUpgradeSalvationLevelInput");
+
+const simOptionsResetButton = document.getElementById("simOptionsResetButton");
+
+const bulkSimOptionsIterationsInput = document.getElementById("bulkSimOptionsIterationsInput");
+
+const bulkSimActionsStartAbortButton = document.getElementById("bulkSimActionsStartAbortButton");
+
+const bulkSimResult = document.getElementById("bulkSimResult");
+
+const averageTracesUsedValue = document.getElementById("averageTracesUsedValue");
+const averageMesosUsedOnTracesValue = document.getElementById("averageMesosUsedOnTracesValue");
+const medianTracesUsedValue = document.getElementById("medianTracesUsedValue");
+const medianMesosUsedOnTracesValue = document.getElementById("medianMesosUsedOnTracesValue");
+const tracesUsedIqrValue = document.getElementById("tracesUsedIqrValue");
+const mesosUsedOnTracesIqrValue = document.getElementById("mesosUsedOnTracesIqrValue");
+
+const averageCssUsedValue = document.getElementById("averageCssUsedValue");
+const averageMesosUsedOnCssValue = document.getElementById("averageMesosUsedOnCssValue");
+const medianCssUsedValue = document.getElementById("medianCssUsedValue");
+const medianMesosUsedOnCssValue = document.getElementById("medianMesosUsedOnCssValue");
+const cssUsedIqrValue = document.getElementById("cssUsedIqrValue");
+const mesosUsedOnCssIqrValue = document.getElementById("mesosUsedOnCssIqrValue");
+
+const averageInnoUsedValue = document.getElementById("averageInnoUsedValue");
+const averageMesosUsedOnInnoValue = document.getElementById("averageMesosUsedOnInnoValue");
+const medianInnoUsedValue = document.getElementById("medianInnoUsedValue");
+const medianMesosUsedOnInnoValue = document.getElementById("medianMesosUsedOnInnoValue");
+const innoUsedIqrValue = document.getElementById("innoUsedIqrValue");
+const mesosUsedOnInnoIqrValue = document.getElementById("mesosUsedOnInnoIqrValue");
+
+const averageTotalMesosUsedValue = document.getElementById("averageTotalMesosUsedValue");
+const medianTotalMesosUsedValue = document.getElementById("medianTotalMesosUsedValue");
+const totalMesosUsedIqrValue = document.getElementById("totalMesosUsedIqrValue");
+
+const customPercentileStatsInput = document.getElementById("customPercentileStatsInput");
+const customPercentileSubmitButton = document.getElementById("customPercentileSubmitButton");
+
+const customTracesUsedPercentile = document.getElementById("customTracesUsedPercentile");
+const customMesosUsedOnTracesPercentile = document.getElementById("customMesosUsedOnTracesPercentile");
+const customCssUsedPercentile = document.getElementById("customCssUsedPercentile");
+const customMesosUsedOnCssPercentile = document.getElementById("customMesosUsedOnCssPercentile");
+const customInnoUsedPercentile = document.getElementById("customInnoUsedPercentile");
+const customMesosUsedOnInnoPercentile = document.getElementById("customMesosUsedOnInnoPercentile");
+const customTotalMesosUsedPercentile = document.getElementById("customTotalMesosUsedPercentile");
+
+const customTracesUsedPercentileValue = document.getElementById("customTracesUsedPercentileValue");
+const customMesosUsedOnTracesPercentileValue = document.getElementById("customMesosUsedOnTracesPercentileValue");
+const customCssUsedPercentileValue = document.getElementById("customCssUsedPercentileValue");
+const customMesosUsedOnCssPercentileValue = document.getElementById("customMesosUsedOnCssPercentileValue");
+const customInnoUsedPercentileValue = document.getElementById("customInnoUsedPercentileValue");
+const customMesosUsedOnInnoPercentileValue = document.getElementById("customMesosUsedOnInnoPercentileValue");
+const customTotalMesosUsedPercentileValue = document.getElementById("customTotalMesosUsedPercentileValue");
