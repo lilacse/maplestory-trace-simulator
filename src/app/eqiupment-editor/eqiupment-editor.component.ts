d10e03e13256df4a29ab81f2b16b77fb91429914
Ang Jin Hang (Lilacse) <rirakusu@protonmail.com>
2022-05-07 13:34:40 +0800

    Simplified access to equipment config in code.

--- src/app/eqiupment-editor/eqiupment-editor.component.ts
+++ src/app/eqiupment-editor/eqiupment-editor.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import Equipment from '../trace-simulator/modules/equipment/Equipment';
 import { TraceSimulatorService } from '../trace-simulator/trace-simulator.service';
 
 @Component({
@@ -13,21 +14,25 @@ export class EqiupmentEditorComponent implements OnInit {
   private readonly _defaultEquipInitialFailedSlots: number;
   private readonly _defaultEquipCostPerClick: number;
 
+  public equipRef: Equipment;
+
   constructor(public traceSimulatorService: TraceSimulatorService) {
-    this._defaultEquipTotalSlots = traceSimulatorService.equipment.totalSlotCount;
-    this._defaultEquipInitialSucceededSlots = traceSimulatorService.equipment.succededSlotCount;
-    this._defaultEquipInitialFailedSlots = traceSimulatorService.equipment.failedSlotCount;
-    this._defaultEquipCostPerClick = traceSimulatorService.equipment.costPerClick;
+    this.equipRef = traceSimulatorService.equipment;
+
+    this._defaultEquipTotalSlots = this.equipRef.totalSlotCount;
+    this._defaultEquipInitialSucceededSlots = this.equipRef.succededSlotCount;
+    this._defaultEquipInitialFailedSlots = this.equipRef.failedSlotCount;
+    this._defaultEquipCostPerClick = this.equipRef.costPerClick;
   }
 
   ngOnInit(): void {
   }
 
   public resetEquipOptions(): void {
-    this.traceSimulatorService.equipment.totalSlotCount = this._defaultEquipTotalSlots;
-    this.traceSimulatorService.equipment.succededSlotCount = this._defaultEquipInitialSucceededSlots;
-    this.traceSimulatorService.equipment.failedSlotCount = this._defaultEquipInitialFailedSlots;
-    this.traceSimulatorService.equipment.costPerClick = this._defaultEquipCostPerClick;
+    this.equipRef.totalSlotCount = this._defaultEquipTotalSlots;
+    this.equipRef.succededSlotCount = this._defaultEquipInitialSucceededSlots;
+    this.equipRef.failedSlotCount = this._defaultEquipInitialFailedSlots;
+    this.equipRef.costPerClick = this._defaultEquipCostPerClick;
   }
 
 }
